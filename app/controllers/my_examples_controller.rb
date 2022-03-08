4af2a0a65056c318ccaf0cb45c20baa8ef2f0777
Ryan Day <radday1@gmail.com>
2022-03-08 09:38:41 -0700

    fourtune app

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,35 @@
 class MyExamplesController < ApplicationController
+  def fortune_method
+    fortune = [
+      "You are wise",
+      "You are lucky",
+      "Play the lotto",
+    ]
+
+    #   render html: "The fortune is #{fortune[rand(fortune.length)]}"
+    # end
+    lucky_number = []
+    5.times do lucky_number << rand(60) end
+
+    render html: "Your luck number is #{lucky_number} and your fortune is #{fortune.sample}"
+  end
+
+  def ninetynine_method
+    str = ""
+
+    i = 99
+    while i > 2
+      str += "#{i} bottles of beer on the wall, #{i} bottles of beer.
+    Take one down and pass it around, #{i - 1} bottles of beer on the wall."
+      i -= 1
+    end
+    str += "2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall."
+    str += "1 bottle of beer on the wall, 1 bottle of beer.
+    Take one down and pass it around, no more bottles of beer on the wall.
+    
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall."
+    render html: "#{str}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/fortune", controller: "my_examples", action: "fortune_method"
+  get "/beer", controller: "my_examples", action: "ninetynine_method"
 end
